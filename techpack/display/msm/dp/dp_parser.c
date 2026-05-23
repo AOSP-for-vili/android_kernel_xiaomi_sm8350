b7510fac2bcc2b7f3ec73954ccd1d9e74da08bc9
RobertGarciaa <chae0218@naver.com>
2026-05-23 13:25:50 +0900

    Revert "Merge tag 'LA.UM.9.14.1.r1-21100-QCM6490.QISI15.0' of https://git.codelinaro.org/clo/la/platform/vendor/opensource/display-drivers into android13-5.4-lahaina"
    This reverts commit 3700e67df39df78bfbd0a0b0f77574c1a638cb34, reversing
    changes made to be47524e565e267e3bab3837afdbfc1ea4652a71.

--- techpack/display/msm/dp/dp_parser.c
+++ techpack/display/msm/dp/dp_parser.c
@@ -178,10 +178,13 @@ static int dp_parser_misc(struct dp_parser *parser)
 				&parser->pixel_base_off[i]);
 	}
 
-	parser->display_type = of_get_property(of_node, "qcom,display-type",
-					NULL);
-	if (!parser->display_type)
-		parser->display_type = "unknown";
+	parser->display_type = of_get_property(of_node, "qcom,display-type", NULL);
+	if (!parser->display_type) {
+		if (parser->is_edp)
+			parser->display_type = "primary";
+		else
+			parser->display_type = "secondary";
+	}
 
 	parser->panel_notifier_support = of_property_read_bool(of_node,
 			"qcom,panel-notifier-support");
